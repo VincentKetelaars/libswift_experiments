513c60549d9f6fa82b0626a8415e21cd41440093
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-08-23 15:16:46 +0200

    Included a test.cpp file. The problem that first needs to be solved is the linking of libswift with the executable for this project.

--- src/cfiles/main.cpp
+++ src/cfiles/main.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include <swift.h>
 
 #include "main.h"
@@ -7,7 +9,8 @@ using namespace swift;
 
 int main(int argc, char *argv[])
 {
+	fprintf(stdout,"Started main program!\n");
 	LibraryInit();
-	Address* address = Address("127.0.0.1:8080");
-	Listen(address)
+	Address address = Address("127.0.0.1:8080");
+	Listen(address);
 }
