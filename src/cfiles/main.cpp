9c6f264f327c36738119522a6c5d7bb6830db2f7
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-08-23 16:28:38 +0200

    Given that the libswift folder has a library named "ctest" the Makefile will now succesfully link the executable runner to necessary objects.

--- src/cfiles/main.cpp
+++ src/cfiles/main.cpp
@@ -13,4 +13,6 @@ int main(int argc, char *argv[])
 	LibraryInit();
 	Address address = Address("127.0.0.1:8080");
 	Listen(address);
+	Open("/home/vincent/Downloads/tutorial.pdf");
+	fprintf(stdout,"Done!\n");
 }
