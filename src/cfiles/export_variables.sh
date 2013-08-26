fabf879893bef074acd23def9741634c9f220911
Vincent Ketelaars <vincentketelaars@gmail.com>
2013-08-26 09:58:09 +0200

    Added README. export_variables.sh exports the LD_LIBRARY_PATH

---
+++ src/cfiles/export_variables.sh
@@ -0,0 +1,4 @@
+SWIFT_HOME=/home/vincent/svn/libswift/ppsp
+
+# Export LD_LIBRARY_PATH, otherwise ld will not be able to find SWIFT_LIB
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SWIFT_HOME
--- src/cfiles/main.cpp
+++ src/cfiles/main.cpp
@@ -7,11 +7,13 @@
 using namespace std;
 using namespace swift;
 
+const char* ADDRESS_1 = "127.0.0.1:8080";
+
 int main(int argc, char *argv[])
 {
 	fprintf(stdout,"Started main program!\n");
 	LibraryInit();
-	Address address = Address("127.0.0.1:8080");
+	Address address = Address(ADDRESS_1);
 	Listen(address);
 	Open("/home/vincent/Downloads/tutorial.pdf");
 	fprintf(stdout,"Done!\n");
